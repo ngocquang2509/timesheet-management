bf474b9cbefa762d7148c466bc2f6168c8a0b7ce
Ngoc Quang Nguyen <ngoc.nguyenquang@asnet.com.vn>
2022-11-29 16:07:38 +0700

    Inline If-Else with conditional operator

--- my-react-app/src/Main Concepts/Conditional Rendering.tsx
+++ my-react-app/src/Main Concepts/Conditional Rendering.tsx
@@ -1,21 +1,8 @@
 export const Greeting = (props: any) => {
   const isLoggedIn = props.isLoggedIn;
-  let AuthBtn;
-  if (isLoggedIn) {
-    AuthBtn = <button>Logout</button>;
-    return (
-      <div>
-        <h1>Hello user</h1>
-        {AuthBtn}
-      </div>
-    );
-  } else {
-    AuthBtn = <button>Login</button>;
-    return (
-      <div>
-        <h1>Hello guest</h1>
-        {AuthBtn}
-      </div>
-    );
-  }
+  return (
+    <div>
+      <h1>The current user is: {isLoggedIn ? "Logged User" : "Guest"}</h1>
+    </div>
+  );
 };
