7e9f013217c7eb83bc77a0ee3542c252a7e0c131
Ngoc Quang Nguyen <ngoc.nguyenquang@asnet.com.vn>
2022-12-08 16:27:22 +0700

    #15 Add size for button component

--- practices/practice-one/timesheet-management/src/components/Button/index.tsx
+++ practices/practice-one/timesheet-management/src/components/Button/index.tsx
@@ -1,5 +1,6 @@
 interface IButton extends React.ButtonHTMLAttributes<HTMLButtonElement> {
   textContent?: string;
+  size?: string;
   bgColor?: string;
   onClick?: () => void;
 }
@@ -7,11 +8,14 @@ interface IButton extends React.ButtonHTMLAttributes<HTMLButtonElement> {
 export const Button = ({
   textContent,
   bgColor,
+  size,
   onClick,
   ...rest
 }: IButton): React.ReactElement => (
   <button
-    className={`${bgColor} text-white text-center px-5 py-2.5 mr-2 mb-2 rounded-lg`}
+    className={`${bgColor} text-white text-center px-5 py-2.5 mr-2 mb-2 rounded-lg ${
+      size === 'sm' ? 'text-sm' : size === 'md' ? 'text-md' : 'text-lg'
+    }`}
     onClick={onClick}
     {...rest}
   >
