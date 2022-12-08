02b3c594ef057f6881db95bcb7082754aad55a8c
Ngoc Quang Nguyen <ngoc.nguyenquang@asnet.com.vn>
2022-12-08 17:55:32 +0700

    #15 Update button component

--- practices/practice-one/timesheet-management/src/components/Button/index.tsx
+++ practices/practice-one/timesheet-management/src/components/Button/index.tsx
@@ -1,6 +1,12 @@
+export enum ButtonSize {
+  SMALL = 'text-sm',
+  MEDIUM = 'text-md',
+  LARGE = 'text-lg'
+}
+
 interface IButton extends React.ButtonHTMLAttributes<HTMLButtonElement> {
   textContent?: string;
-  size?: string;
+  size?: ButtonSize;
   bgColor?: string;
   onClick?: () => void;
 }
@@ -8,15 +14,12 @@ interface IButton extends React.ButtonHTMLAttributes<HTMLButtonElement> {
 export const Button = ({
   textContent,
   bgColor,
-  size = 'md',
+  size = ButtonSize.MEDIUM,
   onClick,
   ...rest
 }: IButton): React.ReactElement => (
   <button
-    className={`${bgColor} text-white text-center px-5 py-2.5 mr-2 mb-2 rounded-lg ${
-      size === 'sm' ? 'text-sm' : size === 'lg' ? 'text-lg' : size
-    }`}
-
+    className={`${size} ${bgColor} text-white text-center px-5 py-2.5 mr-2 mb-2 rounded-lg `}
     onClick={onClick}
     {...rest}
   >
