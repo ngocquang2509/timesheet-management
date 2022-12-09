983b6474ecebf1da16ae1c58e68d25ef7bf909c4
Ngoc Quang Nguyen <ngoc.nguyenquang@asnet.com.vn>
2022-12-09 15:07:50 +0700

    #15 Update button component

--- practices/practice-one/timesheet-management/src/components/Button/index.tsx
+++ practices/practice-one/timesheet-management/src/components/Button/index.tsx
@@ -15,21 +15,14 @@ export const Button = ({
   const baseClass = 'text-white text-center px-5 py-2.5 mr-2 mb-2 rounded-lg';
   const classes = [
     baseClass,
-    size === 'sm'
-      ? 'text-sm'
-      : '' || size === 'md'
-      ? 'text-md'
-      : '' || size === 'lg'
-      ? 'text-lg'
-      : '',
 
-    bgColor === 'primary'
-      ? 'bg-sky-500 hover:bg-sky-600'
-      : '' || bgColor === 'info'
-      ? 'bg-green-500 hover:bg-green-600'
-      : '' || bgColor === 'danger'
-      ? 'bg-red-500 hover:bg-red-600'
-      : ''
+    size === 'sm' ? 'text-sm' : '',
+    size === 'md' ? 'text-md' : '',
+    size === 'lg' ? 'text-lg' : '',
+
+    bgColor === 'primary' ? 'bg-sky-500 hover:bg-sky-600' : '',
+    bgColor === 'info' ? 'bg-green-500 hover:bg-green-600' : '',
+    bgColor === 'danger' ? 'bg-red-500 hover:bg-red-600' : ''
   ].join(' ');
 
   return (
