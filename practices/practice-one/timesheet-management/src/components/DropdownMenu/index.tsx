55653a706b47b2a1d82bbff09167ff4d60a8d9a1
Ngoc Quang Nguyen <ngoc.nguyenquang@asnet.com.vn>
2022-12-09 11:04:06 +0700

    #18 Remove size props

--- practices/practice-one/timesheet-management/src/components/DropdownMenu/index.tsx
+++ practices/practice-one/timesheet-management/src/components/DropdownMenu/index.tsx
@@ -2,10 +2,9 @@ import { DropdownOption } from 'interfaces/DropdownOption';
 
 interface IDropdownMenu extends Omit<React.ComponentPropsWithoutRef<'select'>, 'size'> {
   options: DropdownOption[];
-  size?: 'small' | 'medium' | 'large';
 }
 
-const DropdownMenu = ({ options, size, ...selectProps }: IDropdownMenu): React.ReactElement => (
+const DropdownMenu = ({ options, ...selectProps }: IDropdownMenu): React.ReactElement => (
   <select
     className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-2/4 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
     {...selectProps}
