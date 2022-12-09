c1cd3bad7e8062836f9e1605d61fbba5da880838
Ngoc Quang Nguyen <ngoc.nguyenquang@asnet.com.vn>
2022-12-09 11:03:02 +0700

    #18 Remove size props in Dropdown

--- practices/practice-one/timesheet-management/src/pages/Home/index.tsx
+++ practices/practice-one/timesheet-management/src/pages/Home/index.tsx
@@ -1,9 +1,19 @@
-import Header from 'layouts/Header';
+import DropdownMenu from '../../components/DropdownMenu';
+import { OPTIONS_RECORD } from '../../constants/dropdown-option';
+import Header from '../../layouts/Header';
 
-const Home = (): React.ReactElement => (
-  <>
-    <Header />
-  </>
-);
+const Home = (): React.ReactElement => {
+  const handleClick = (e: any) => {
+    e.preventDefault();
+    console.log('Clicked');
+  };
+
+  return (
+    <>
+      <Header />
+      <DropdownMenu options={OPTIONS_RECORD} />
+    </>
+  );
+};
 
 export default Home;
