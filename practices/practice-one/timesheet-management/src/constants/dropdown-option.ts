27b1edb603f24eeebc62db438c2fa1bc3e53765b
Ngoc Quang Nguyen <ngoc.nguyenquang@asnet.com.vn>
2022-12-09 11:01:13 +0700

    #18 Set options value

---
+++ practices/practice-one/timesheet-management/src/constants/dropdown-option.ts
@@ -0,0 +1,7 @@
+import { DropdownOption } from "interfaces/DropdownOption";
+
+export const OPTIONS_RECORD: DropdownOption[] = [
+  { value: 'Reading', text: 'Reading' },
+  { value: 'Practice', text: 'Practice' },
+  { value: 'Exercise', text: 'Exercise' }
+]
