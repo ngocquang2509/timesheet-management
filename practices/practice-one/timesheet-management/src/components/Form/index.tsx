c7ad16fdbb53d73cb2f2940f62908bac4c381dcc
Ngoc Quang Nguyen <ngoc.nguyenquang@asnet.com.vn>
2022-12-12 17:00:46 +0700

    #25 Implement add new Timesheet

--- practices/practice-one/timesheet-management/src/components/Form/index.tsx
+++ practices/practice-one/timesheet-management/src/components/Form/index.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { DROPDOWN_OPTIONS } from 'src/constants/dropdownOption';
 import { DropdownOption } from 'src/interfaces/DropdownOption';
 import Button from 'components/Button';
@@ -8,22 +8,33 @@ import Input from 'components/Input';
 interface IForm {
   hours: string;
   comments: string;
-  records: DropdownOption
+  records: string;
 }
 
-const Form = (): React.ReactElement => {
-  const [values, setValues] = useState<IForm>({
-    hours: '',
-    comments: '',
-    records: DROPDOWN_OPTIONS
-  });
+const getFormValues = () => {
+  const storeValues = localStorage.getItem('time-sheet');
+  if (!storeValues)
+    return {
+      hours: '',
+      comments: '',
+      records: DROPDOWN_OPTIONS[0].value
+    };
+  return JSON.parse(storeValues);
+};
 
-  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
-    //TODO: handle submit form 
+const Form = (): React.ReactElement => {
+  const [values, setValues] = useState<IForm>(getFormValues);
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement | HTMLSelectElement>) => {
+    setValues((preValues) => ({
+      ...preValues,
+      [e.target.name]: e.target.value
+    }));
   };
 
-  const handleChange = (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
-    //TODO: handle input change
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    localStorage.setItem('time-sheet', JSON.stringify(values));
   };
 
   return (
@@ -31,22 +42,25 @@ const Form = (): React.ReactElement => {
       <form className="grid grid-cols-6 pb-8" onSubmit={handleSubmit}>
         <div className="col-span-2 row-start-2">
           <label>Hours</label>
-          <Input name='hours' type="text" size="md" value={values.hours} onChange={handleChange} />
+          <Input name="hours" type="text" size="md" value={values.hours} onChange={handleChange} />
         </div>
 
         <div className="col-start-3 col-span-3 mb-5">
           <label>Records</label>
-          <DropdownMenu options={DROPDOWN_OPTIONS} />
+          <DropdownMenu options={DROPDOWN_OPTIONS} onChange={handleChange}/>
         </div>
 
         <div className="row-start-2 col-span-4">
           <label>Comments</label>
           <textarea
             className="form-control block w-full px-3 py-1.5 text-baseb font-normal text-gray-700 bg-white bg-clip-padding border border-solid border-gray-300 rounded transition ease-in-out m-0 focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
-            name='comments'
+            name="comments"
             value={values.comments}
             onChange={handleChange}
           />
+        </div>
+        <div className='row-start-3 col-start-6 mt-4'>
+          <Button size="md" bgColor="primary" textContent="Submit" />
         </div>
       </form>
     </>
