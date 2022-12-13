df4eba92a4be08302d3242d5cd1c3aa1cbf6e4fc
Ngoc Quang Nguyen <ngoc.nguyenquang@asnet.com.vn>
2022-12-13 09:56:19 +0700

    #25 Update code comment

--- practices/practice-one/timesheet-management/src/helpers/localStore.ts
+++ practices/practice-one/timesheet-management/src/helpers/localStore.ts
@@ -2,7 +2,11 @@
  * Helper to working with LocalStorage
  */
 export const StorageHelper = {
-  // Get data from Local Storage
+  /**
+   * Get data from LocalStorage
+   * @param {String} key: key name of LocalStorage 
+   * @returns: return data from LocalStorage
+   */
   get: (key: string) => {
     try {
       let data = JSON.parse(localStorage.getItem(key) || '');
@@ -16,7 +20,11 @@ export const StorageHelper = {
     }
   },
 
-  // Save a new data to Local Storage
+  /**
+   * Save a new data to Local Storage
+   * @param {String} key: key name of LocalStorage 
+   * @param {any} value: data that saved to LocalStorage
+   */
   set: (key: string, value: any) => {
     localStorage.setItem(key, JSON.stringify(value));
   }
